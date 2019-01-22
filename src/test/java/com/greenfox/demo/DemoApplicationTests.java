534491bc9b5daca7963ed26ce93675fe3fb5ac77
RBKoronczi <rbkoronczi@gmail.com>
2019-01-22 16:49:35 +0100

    Added false test as practice

--- src/test/java/com/greenfox/demo/DemoApplicationTests.java
+++ src/test/java/com/greenfox/demo/DemoApplicationTests.java
@@ -5,13 +5,19 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertTrue;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+	@Test
+	public void contextLoads() {
+	}
 
+	@Test
+	public void falseItShouldBe() {
+		assertTrue(false);
+	}
 }
 
